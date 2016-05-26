dd1e5b92baf228febd1f9b5762705bb3c3aac0a9
David Wagner <david.wagner@intel.com>
2016-05-26 18:25:21 +0200

    Add StructureVisitor::visit overloads for stock field types
    Change-Id: I96727a873c121e7ec17276be16c79b4a7bee6762
    Signed-off-by: David Wagner <david.wagner@intel.com>

---
+++ structure/Visitor.cpp
@@ -0,0 +1,21 @@
+#include "Visitor.hpp"
+#include "Field.hpp"
+
+namespace structure
+{
+// The various field types need to be forward-declared Visitor.hpp. As a result, the fact that they
+// derive from GenericField is unknown by the compiler and so, the static_cast does not compile.
+// This is why the definitions for these fallbacks need to be in their own cpp.
+void StructureVisitor::visit(const Integer &i)
+{
+    visit(static_cast<const GenericField &>(i));
+}
+void StructureVisitor::visit(const Float &f)
+{
+    visit(static_cast<const GenericField &>(f));
+}
+void StructureVisitor::visit(const String &s)
+{
+    visit(static_cast<const GenericField &>(s));
+}
+}
--- structure/include/Visitor.hpp
+++ structure/include/Visitor.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "StockTypes.hpp"
+
 namespace structure
 {
 
@@ -15,6 +17,10 @@ public:
 
     virtual void visit(const Block &block) = 0;
     virtual void visit(const GenericField &field) = 0;
+
+    virtual void visit(const Integer &i);
+    virtual void visit(const Float &f);
+    virtual void visit(const String &s);
 };
 
 class ValueVisitor
@@ -24,5 +30,6 @@ public:
 
     virtual void visit(const BlockValue &block) = 0;
     virtual void visit(const GenericFieldValue &field) = 0;
+    // TODO: overloads for stock value types
 };
 }
