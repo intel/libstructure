8342a091fb7a2e9286a399c1fb059e5878a853e6
David Wagner <david.wagner@intel.com>
2016-06-23 11:04:04 +0200

    Tests: factorize some code
    Change-Id: I31be5787d3340a4df4c363c9c4f22307a9867734
    Signed-off-by: David Wagner <david.wagner@intel.com>

--- test/unit-tests/import.cpp
+++ test/unit-tests/import.cpp
@@ -18,45 +18,40 @@ SCENARIO("Importing values with ValueImporter subclasses", "[value][import]")
 {
     GIVEN ("A valid structure ") {
         auto root = Block("root", Int8("a"), Block("b", Float("c"), Int8("d")), Int8("e"));
+        std::unique_ptr<StructureValue> value;
         std::stringstream null;
         std::stringstream ss;
 
         GIVEN ("A StreamImporter") {
+            auto importer = StreamImporter<>(ss);
             THEN ("Creating a value from correct input should not throw.") {
                 ss << "1 2.3 4 5";
-                auto importer = StreamImporter<>(ss);
-                std::unique_ptr<StructureValue> value;
                 CHECK_NOTHROW(value = build(root, importer));
                 CHECK(getValue(value) == "{1, {2.300000, 4}, 5}");
             }
             THEN ("Creating a value from erroneous input should throw.") {
                 ss << "1.2 3 3";
-                auto importer = StreamImporter<>(ss);
                 CHECK_THROWS(build(root, importer));
             }
             THEN ("Creating a value from partial input should throw.") {
                 ss << "1 2.3";
-                auto importer = StreamImporter<>(ss);
                 CHECK_THROWS(build(root, importer));
             }
         }
 
         GIVEN ("A PromptImporter") {
+            auto importer = PromptImporter<>(ss, null);
             THEN ("Creating a value from correct input should not throw.") {
                 ss << "1 2.3 4 5";
-                auto importer = PromptImporter<>(ss, null);
-                std::unique_ptr<StructureValue> value;
                 CHECK_NOTHROW(value = build(root, importer));
                 CHECK(getValue(value) == "{1, {2.300000, 4}, 5}");
             }
             THEN ("Creating a value from erroneous input should throw.") {
                 ss << "1.0 2.3 4";
-                auto importer = PromptImporter<>(ss, null);
                 CHECK_THROWS(build(root, importer));
             }
             THEN ("Creating a value from partial input should throw.") {
                 ss << "1 2.3";
-                auto importer = PromptImporter<>(ss, null);
                 CHECK_THROWS(build(root, importer));
             }
         }
@@ -66,7 +61,6 @@ SCENARIO("Importing values with ValueImporter subclasses", "[value][import]")
                 std::map<std::string, std::string> values = {
                     {"/root/a", "1"}, {"/root/b/c", "2.3"}, {"/root/b/d", "4"}, {"/root/e", "5"}};
                 auto importer = MapImporter(values);
-                std::unique_ptr<StructureValue> value;
                 CHECK_NOTHROW(value = build(root, importer));
                 CHECK(getValue(value) == "{1, {2.300000, 4}, 5}");
             }
@@ -101,7 +95,6 @@ SCENARIO("Importing values with ValueImporter subclasses", "[value][import]")
 
                 ss << "1 2.3 4 5";
                 CustomImporter importer(ss);
-                std::unique_ptr<StructureValue> value;
                 CHECK_NOTHROW(value = build(root, importer));
                 CHECK(getValue(value) == "{1, {2.300000, 4}, 5}");
                 CHECK(importer.onEnterBlockCount == 2);
@@ -110,10 +103,9 @@ SCENARIO("Importing values with ValueImporter subclasses", "[value][import]")
         }
 
         GIVEN ("Mixed literal and stream importers") {
+            StreamImporter<> importer(ss);
             THEN ("Creating a value from correct input should work") {
-                std::stringstream ss("2.3 4");
-                StreamImporter<> importer(ss);
-                std::unique_ptr<StructureValue> value;
+                ss << "2.3 4";
                 CHECK_NOTHROW(value = root.with({"1", importer, "5"}));
                 CHECK(getValue(value) == "{1, {2.300000, 4}, 5}");
             }
